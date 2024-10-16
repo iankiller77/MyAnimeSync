0a28d559323c7d298fe95f3d2f77ad2ee8b29b5f
iankiller77 <iankiller77@hotmail.com>
2024-10-16 18:24:35 -0400

    Refactor OnMarkedService to extract update logic from OnUserDataMarkedPlay to it's own function called UpdateAnimeList

--- Jellyfin.Plugin.MyAnimeSync/Service/OnMarkedService.cs
+++ Jellyfin.Plugin.MyAnimeSync/Service/OnMarkedService.cs
@@ -95,6 +95,118 @@ namespace Jellyfin.Plugin.MyAnimeSync.Service
         /// <summary>
         /// Update anime watch list on MyAnimeList when an episode is marked as watched.
         /// </summary>
+        /// <param name="serie">The serie's name.<see cref="string"/>.</param>
+        /// <param name="episodeNumber">The episode number.<see cref="int"/>.</param>
+        /// <param name="seasonNumber">The season number.<see cref="int"/>.</param>
+        /// <param name="userConfig">The user config. <see cref="UserConfig"/>.</param>
+        /// <returns> The task. </returns>
+        public async Task<bool> UpdateAnimeList(string serie, int episodeNumber, int? seasonNumber, UserConfig userConfig)
+        {
+            // Update tokens if needed before using the api.
+            await MalApiHandler.RefreshTokens(userConfig).ConfigureAwait(true);
+
+            int? id = await MalApiHandler.GetAnimeID(serie, userConfig).ConfigureAwait(true);
+            if (id == null)
+            {
+                _logger.LogError(
+                    "Could not retrieve id for anime : {AnimeName}",
+                    serie);
+                return false;
+            }
+
+            AnimeData? info = await MalApiHandler.GetAnimeInfo(id.Value, userConfig).ConfigureAwait(true);
+            if (info == null || info.ID == null || info.EpisodeCount == null)
+            {
+                _logger.LogError(
+                    "Could not retrieve anime info for id : {ID}",
+                    id);
+                return false;
+            }
+
+            int seasonOffset = seasonNumber - 1 ?? 0;
+
+            // If we have a specified anime season.
+            while (seasonOffset > 0)
+            {
+                info = await GetAnimeSequel(info, userConfig).ConfigureAwait(true);
+                if (info == null || info.ID == null || info.EpisodeCount == null || info.Title == null || info.MediaType == null)
+                {
+                    _logger.LogError(
+                        "Could not retrieve expected sequel using season offset for anime : {ID}",
+                        id);
+                    return false;
+                }
+
+                // Ignore anime movie for season offset.
+                if (info.MediaType == MediaType.Movie)
+                {
+                    continue;
+                }
+
+                Regex expression = new Regex(".*part ([0-9]+).*", RegexOptions.IgnoreCase);
+                Match match = expression.Match(info.Title);
+                if (match.Success)
+                {
+                    int partNumber;
+                    _ = int.TryParse(match.Groups[1].Value, out partNumber);
+                    if (partNumber > 1)
+                    {
+                        continue;
+                    }
+                }
+
+                seasonOffset--;
+            }
+
+            // If episode is > expect max season episode, we try to find the proper season.
+            // Also if the number of episodes is unknown, result is 0. So treat it as being the proper anime season.
+            while (info.EpisodeCount > 0 && info.EpisodeCount < episodeNumber)
+            {
+                episodeNumber -= info.EpisodeCount.Value;
+                info = await GetAnimeSequel(info, userConfig).ConfigureAwait(true);
+                if (info == null || info.ID == null || info.EpisodeCount == null)
+                {
+                    _logger.LogError(
+                        "Could not retrieve expected sequel using episode offset for anime : {ID}",
+                        id);
+                    return false;
+                }
+            }
+
+            // Retrieve anime status in user library.
+            UserAnimeInfo entry = await MalApiHandler.GetUserAnimeInfo(info.ID.Value, userConfig).ConfigureAwait(true);
+            if (!entry.SuccessStatus)
+            {
+                _logger.LogError(
+                        "Could not parse anime list for user : {User}",
+                        userConfig.Id);
+                return false;
+            }
+
+            if (entry.Info != null && entry.Info.StatusInfo != null)
+            {
+                // Do nothing if anime is already marked as completed or if the episode number is not higher then the user watched episode.
+                if (entry.Info.StatusInfo.Status == WatchStatus.Completed || entry.Info.StatusInfo.EpisodeWatched >= episodeNumber)
+                {
+                    _logger.LogInformation("No need to update user entry for anime : {Anime} season {Season} ep {Ep}", serie, seasonNumber, entry.Info.StatusInfo.EpisodeWatched);
+                    return true;
+                }
+            }
+
+            string status = WatchStatus.Watching;
+            if (info.EpisodeCount > 0 && episodeNumber >= info.EpisodeCount)
+            {
+                status = WatchStatus.Completed;
+            }
+
+            // Update anime status
+            MalApiHandler.UpdateUserInfo(info.ID.Value, episodeNumber, status, userConfig);
+            return true;
+        }
+
+        /// <summary>
+        /// Update anime watch list on MyAnimeList when an episode is marked as watched.
+        /// </summary>
         /// <param name="sender">Sender.<see cref="object"/>.</param>
         /// <param name="eventArgs">Informations about the event.<see cref="UserDataSaveEventArgs"/>.</param>
         private async void OnUserDataMarkedPlayed(object? sender, UserDataSaveEventArgs eventArgs)
@@ -113,8 +225,6 @@ namespace Jellyfin.Plugin.MyAnimeSync.Service
                     return;
                 }
 
-                // Update tokens if needed before using the api.
-                await MalApiHandler.RefreshTokens(userConfig).ConfigureAwait(true);
                 if (eventArgs.Item is Episode episode)
                 {
                     string serie = episode.SeriesName;
@@ -133,15 +243,6 @@ namespace Jellyfin.Plugin.MyAnimeSync.Service
                         return;
                     }
 
-                    int? id = await MalApiHandler.GetAnimeID(serie, userConfig).ConfigureAwait(true);
-                    if (id == null)
-                    {
-                        _logger.LogError(
-                            "Could not retrieve id for anime : {AnimeName}",
-                            serie);
-                        return;
-                    }
-
                     int? episodeNumber = episode.IndexNumber;
                     if (episodeNumber == null)
                     {
@@ -151,93 +252,7 @@ namespace Jellyfin.Plugin.MyAnimeSync.Service
                         return;
                     }
 
-                    AnimeData? info = await MalApiHandler.GetAnimeInfo(id.Value, userConfig).ConfigureAwait(true);
-                    if (info == null || info.ID == null || info.EpisodeCount == null)
-                    {
-                        _logger.LogError(
-                            "Could not retrieve anime info for id : {ID}",
-                            id);
-                        return;
-                    }
-
-                    int seasonOffset = episode.AiredSeasonNumber - 1 ?? 0;
-
-                    // If we have a specified anime season.
-                    while (seasonOffset > 0)
-                    {
-                        info = await GetAnimeSequel(info, userConfig).ConfigureAwait(true);
-                        if (info == null || info.ID == null || info.EpisodeCount == null || info.Title == null || info.MediaType == null)
-                        {
-                            _logger.LogError(
-                                "Could not retrieve expected sequel using season offset for anime : {ID}",
-                                id);
-                            return;
-                        }
-
-                        // Ignore anime movie for season offset.
-                        if (info.MediaType == MediaType.Movie)
-                        {
-                            continue;
-                        }
-
-                        Regex expression = new Regex(".*part ([0-9]+).*", RegexOptions.IgnoreCase);
-                        Match match = expression.Match(info.Title);
-                        if (match.Success)
-                        {
-                            int partNumber;
-                            _ = int.TryParse(match.Groups[1].Value, out partNumber);
-                            if (partNumber > 1)
-                            {
-                                continue;
-                            }
-                        }
-
-                        seasonOffset--;
-                    }
-
-                    // If episode is > expect max season episode, we try to find the proper season.
-                    // Also if the number of episodes is unknown, result is 0. So treat it as being the proper anime season.
-                    while (info.EpisodeCount > 0 && info.EpisodeCount < episodeNumber)
-                    {
-                        episodeNumber -= info.EpisodeCount;
-                        info = await GetAnimeSequel(info, userConfig).ConfigureAwait(true);
-                        if (info == null || info.ID == null || info.EpisodeCount == null)
-                        {
-                            _logger.LogError(
-                                "Could not retrieve expected sequel using episode offset for anime : {ID}",
-                                id);
-                            return;
-                        }
-                    }
-
-                    // Retrieve anime status in user library.
-                    UserAnimeInfo entry = await MalApiHandler.GetUserAnimeInfo(info.ID.Value, userConfig).ConfigureAwait(true);
-                    if (!entry.SuccessStatus)
-                    {
-                        _logger.LogError(
-                                "Could not parse anime list for user : {User}",
-                                userID);
-                        return;
-                    }
-
-                    if (entry.Info != null && entry.Info.StatusInfo != null)
-                    {
-                        // Do nothing if anime is already marked as completed or if the episode number is not higher then the user watched episode.
-                        if (entry.Info.StatusInfo.Status == WatchStatus.Completed || entry.Info.StatusInfo.EpisodeWatched >= episodeNumber.Value)
-                        {
-                            _logger.LogInformation("No need to update user entry for anime : {Anime} season {Season} ep {Ep}", serie, episode.AiredSeasonNumber, entry.Info.StatusInfo.EpisodeWatched);
-                            return;
-                        }
-                    }
-
-                    string status = WatchStatus.Watching;
-                    if (info.EpisodeCount > 0 && episodeNumber >= info.EpisodeCount)
-                    {
-                        status = WatchStatus.Completed;
-                    }
-
-                    // Update anime status
-                    MalApiHandler.UpdateUserInfo(info.ID.Value, episodeNumber.Value, status, userConfig);
+                    bool success = await UpdateAnimeList(serie, episodeNumber.Value, episode.AiredSeasonNumber, userConfig).ConfigureAwait(true);
                 }
             }
         }
