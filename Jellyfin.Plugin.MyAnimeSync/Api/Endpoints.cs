e299cfbf4ad818223b3cabc77f792f4fa68ab151
iankiller77 <iankiller77@hotmail.com>
2025-06-13 00:12:30 -0400

    Add endpoint for full anime list sync using jellyfin watch status

--- Jellyfin.Plugin.MyAnimeSync/Api/Endpoints.cs
+++ Jellyfin.Plugin.MyAnimeSync/Api/Endpoints.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Authentication;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using Jellyfin.Plugin.MyAnimeSync.Api.Mal;
 using Jellyfin.Plugin.MyAnimeSync.Configuration;
 using Jellyfin.Plugin.MyAnimeSync.Service;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -18,14 +24,20 @@ namespace Jellyfin.Plugin.MyAnimeSync.Endpoints
     public class Endpoints : ControllerBase
     {
         private readonly ILogger<Endpoints> _logger;
+        private readonly ILibraryManager _libraryManager;
+        private readonly IUserDataManager _userDataManager;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Endpoints"/> class.
         /// </summary>
         /// <param name="logger">Instance of the <see cref="ILogger{Endpoints}"/> interface.</param>
-        public Endpoints(ILogger<Endpoints> logger)
+        /// /// <param name="libraryManager">Instance of the <see cref="ILibraryManager"/> interface.</param>
+        /// /// <param name="userDataManager">Instance of the <see cref="IUserDataManager"/> interface.</param>
+        public Endpoints(ILogger<Endpoints> logger, ILibraryManager libraryManager, IUserDataManager userDataManager)
         {
             _logger = logger;
+            _libraryManager = libraryManager;
+            _userDataManager = userDataManager;
         }
 
         /// <summary>
@@ -81,6 +93,112 @@ namespace Jellyfin.Plugin.MyAnimeSync.Endpoints
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Synchronise entire jellyfin watch status with myanimelist.net.
+        /// </summary>
+        /// <param name="userID">The user config. <see cref="Guid"/>.</param>
+        /// <returns> A task.</returns>
+        [HttpGet("fullUpdate")]
+        public async Task FullLibraryUpdate([FromQuery(Name = "guid")] Guid userID)
+        {
+            UserConfig? uConfig = Plugin.Instance?.Configuration.GetByGuid(userID);
+            if (uConfig == null || string.IsNullOrEmpty(uConfig.UserToken))
+            {
+                return;
+            }
+
+            List<VirtualFolderInfo> virtualFolders = _libraryManager.GetVirtualFolders().FindAll(element => uConfig.ListMonitoredLibraryGuid.Contains(Guid.Parse(element.ItemId)));
+            List<Folder> watchedFolders = new List<Folder>();
+            foreach (VirtualFolderInfo virtualFolder in virtualFolders)
+            {
+                var folderItem = _libraryManager.FindByPath(virtualFolder.Locations[0], true);
+                if (folderItem is Folder)
+                {
+                    Folder? folder = folderItem as Folder;
+                    if (folder == null)
+                    {
+                        continue;
+                    }
+
+                    watchedFolders.Add(folder);
+                }
+            }
+
+            foreach (Folder folder in watchedFolders)
+            {
+                /*
+                    TODO: Investigate improving algorithm to avoid updating all episodes when season is not provided.
+                            Also investigate improving overall code to reuse update logic for multiple anime at the same time.
+                            EX: Create of a list of all animes to update before updating and creating a batch update function to optimize the amount of update requests and reuse code.
+                */
+
+                foreach (var serieItem in folder.Children)
+                {
+                    if (serieItem is Series)
+                    {
+                        Series? serie = serieItem as Series;
+                        if (serie == null)
+                        {
+                            continue;
+                        }
+
+                        foreach (var item in serie.Children)
+                        {
+                            if (item is Episode)
+                            {
+                                Episode? episode = item as Episode;
+                                if (episode == null || episode.IndexNumber == null)
+                                {
+                                    continue;
+                                }
+
+                                bool isPlayed = _userDataManager.GetUserData(userID, episode).Played;
+                                if (isPlayed)
+                                {
+                                    await OnMarkedService.UpdateAnimeList(serie.Name, episode.IndexNumber.Value, episode.AiredSeasonNumber, uConfig, _logger).ConfigureAwait(false);
+                                }
+                            }
+                            else if (item is Season)
+                            {
+                                Season? season = item as Season;
+                                if (season == null)
+                                {
+                                    continue;
+                                }
+
+                                int maxEpisodeNumber = -1;
+                                foreach (var item2 in season.Children)
+                                {
+                                    if (item2 is Episode)
+                                    {
+                                        Episode? episode = item2 as Episode;
+                                        if (episode == null)
+                                        {
+                                            continue;
+                                        }
+
+                                        bool isPlayed = _userDataManager.GetUserData(userID, episode).Played;
+                                        if (isPlayed)
+                                        {
+                                            if (episode.IndexNumber != null && episode.IndexNumber > maxEpisodeNumber)
+                                            {
+                                                maxEpisodeNumber = episode.IndexNumber.Value;
+                                            }
+                                        }
+                                    }
+                                }
+
+                                if (maxEpisodeNumber > 0)
+                                {
+                                    await OnMarkedService.UpdateAnimeList(serie.Name, maxEpisodeNumber, season.IndexNumber, uConfig, _logger).ConfigureAwait(false);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         /// <summary>
